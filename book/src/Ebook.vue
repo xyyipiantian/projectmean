525751bc419bd2140f23a74750618b6a1cc7b3e8
xiangtiandi <591102518@qq.com>
2019-09-24 23:36:42 +0800

    简单epub阅读器

---
+++ book/src/Ebook.vue
@@ -0,0 +1,206 @@
+<!--  -->
+<template>
+  <div class="ebook">
+    <TitleBar :ifTitleAndMenuShow="ifTitleAndMenuShow"></TitleBar>
+    <div class="read-wrapper">
+      <div id="read"></div>
+      <div class="mask">
+        <div class="left" @click="prevPage"></div>
+        <div class="center" @click="toggleTitleAndMenu"></div>
+        <div class="right" @click="nextPage"></div>
+      </div>
+    </div>
+    <MenuBar 
+    :ifTitleAndMenuShow="ifTitleAndMenuShow" 
+    ref='MenuBar' 
+    :fontSizeList='fontSizeList'
+    :defaultFontSize='defaultFontSize' 
+    @setFontSize='setFontSize'
+    :themeList='themeList'
+    :defaultTheme='defaultTheme'
+    @setTheme='setTheme'
+    :bookAvailable='bookAvailable'
+    @onProgressChange='onProgressChange'
+    :navigation="navigation"
+    @jumpTo='jumpTo'></MenuBar>
+  </div>
+</template>
+
+<script>
+import TitleBar from "./components/TitleBar";
+import MenuBar from "./components/MenuBar";
+import Epub from "epubjs";
+const DOWNLOAD_URL = "../static/2018_Book_AgileProcessesInSoftwareEngine.epub";
+export default {
+  data() {
+    return {
+      ifTitleAndMenuShow: false,
+      fontSizeList:[
+          {fontSize:12},
+          {fontSize:14},
+          {fontSize:16},
+          {fontSize:18},
+          {fontSize:20},
+          {fontSize:22},
+          {fontSize:24},
+      ],
+      defaultFontSize:16,
+      defaultTheme:2,
+      themeList:[
+        {
+          name:'defaulet',
+          style:{
+            body:{
+              'color':'#000','background':'#fff'
+            }
+          }
+        },
+        {
+          name:'eye',
+          style:{
+            body:{
+              'color':'#000','background':'#ceeaba'
+            }
+          }
+        },
+        {
+          name:'night',
+          style:{
+            body:{
+              'color':'#fff','background':'#000'
+            }
+          }
+        },
+        {
+          name:'gold',
+          style:{
+            body:{
+              'color':'#000','background':'rgb(241,236,226)'
+            }
+          }
+        },
+      ],
+      bookAvailable:false,
+      navigation: null
+    };
+  },
+  methods: {
+    //根据链接跳转到指定位置
+    jumpTo(href){
+      this.rendition.display(href);
+      this.hideTitleAndMenu()
+    },
+    hideTitleAndMenu(){
+      //隐藏标题栏和菜单栏
+      this.ifTitleAndMenuShow = false;
+      //隐藏菜单栏弹出的设置栏
+      this.$refs.MenuBar.hiddenSetting();
+      //隐藏目录
+      this.$refs.MenuBar.hideContent();
+    },
+    //progress 进度条为0-100
+    onProgressChange(progress){
+      const percentage = progress /100;
+      const location = percentage > 0 ? this.locations.cfiFromPercentage(percentage):0;
+      this.rendition.display(location);
+    },
+    setTheme(index){
+      this.themes.select(this.themeList[index].name);
+      this.defaultTheme = index;
+    },
+    registerTheme(){
+      this.themeList.forEach(theme=>{
+        this.themes.register(theme.name,theme.style)
+      })
+    },
+    setFontSize(fontSize){
+      this.defaultFontSize = fontSize
+      if(this.themes){
+        this.themes.fontSize(fontSize+'px')
+      }
+    },
+    toggleTitleAndMenu() {
+      this.ifTitleAndMenuShow = !this.ifTitleAndMenuShow;
+      if(!this.ifTitleAndMenuShow){
+          this.$refs.MenuBar.hiddenSetting();
+      }
+    },
+    showEpub() {
+      //生成Book
+      this.book = new Epub(DOWNLOAD_URL);
+      //生成Rendition
+      this.rendition = this.book.renderTo("read", {
+        width: window.innerWidth,
+        height: window.innerHeight
+      });
+      //通过Rendtion.display渲染电子书
+      this.rendition.display();
+      //获取Theme对象
+      this.themes = this.rendition.themes
+      //设置默认字体
+      this.setFontSize(this.defaultFontSize)
+      //this.themes.register(name,styles)
+      //this.themes.select(name);
+      this.registerTheme()
+      this.setTheme(this.defaultTheme)
+      //获取locations对象
+      //通过epubjs的钩子函数来实现
+      this.book.ready.then(()=>{
+        this.navigation = this.book.navigation;
+        console.log(this.navigation)
+        return this.book.locations.generate()
+      }).then(result => {
+        this.locations = this.book.locations;
+        this.bookAvailable = true;
+      })
+    },
+    prevPage() {
+      if (this.rendition) {
+        this.rendition.prev();
+        this.ifTitleAndMenuShow = false;
+      }
+    },
+    nextPage() {
+      if (this.rendition) {
+        this.rendition.next();
+        this.ifTitleAndMenuShow = false;
+      }
+    }
+  },
+  components: {
+    TitleBar,
+    MenuBar
+  },
+  mounted() {
+    this.showEpub();
+  }
+};
+</script>
+<style lang='scss'scoped>
+@import "../src/assets/styles/global.scss";
+.ebook {
+  position: relative;
+}
+
+.read-wrapper {
+  .mask {
+    position: absolute;
+    display: flex;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 100;
+    overflow: hidden;
+    .left {
+      flex: 0 0 px2rem(100);
+    }
+    .right {
+      flex: 0 0 px2rem(100);
+    }
+    .center {
+      flex: 1;
+    }
+  }
+}
+</style>
---
+++ book/src/components/TitleBar.vue
@@ -0,0 +1,64 @@
+<!--  -->
+<template>
+  <transition name="slide-down">
+    <div class="title-wrapper" v-show="ifTitleAndMenuShow">
+      <div class="left">
+        <span class="iconfont icon">&#xe7b0;</span>
+      </div>
+      <div class="right">
+        <div class="icon-wrapper">
+          <span class="iconfont icon icon-cart">&#xe639;</span>
+        </div>
+        <div class="icon-wrapper">
+          <span class="iconfont icon">&#xe622;</span>
+        </div>
+        <div class="icon-wrapper">
+          <span class="iconfont icon">&#xe608;</span>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+    props:{
+        ifTitleAndMenuShow:{
+            type:Boolean,
+            default:false,
+        }
+    }
+};
+</script>
+<style lang='scss' scoped>
+@import "../../src/assets/styles/global.scss";
+  .title-wrapper {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    z-index: 101;
+    display: flex;
+    background: white;
+    height: px2rem(48);
+    box-shadow: 0 px2rem(8) px2rem(8) rgba(0, 0, 0, 0.15);
+    .left {
+      flex: 0 0 px2rem(60);
+      @include center;
+      // background:red;
+    }
+    .right {
+      flex: 1;
+      display: flex;
+      justify-content: flex-end;
+      .icon-wrapper {
+        flex: 0 0 px2rem(40);
+        @include center;
+        // background:green;
+        .icon-cart {
+          font-size: px2rem(22);
+        }
+      }
+    }
+  }
+</style>
