769183b932e24c299475f2e2b2e67624697c5fcc
xiangtiandi <591102518@qq.com>
2019-09-25 21:16:27 +0800

    project-init

---
+++ vue去哪网/travel/src/pages/home/Home.vue
@@ -0,0 +1,14 @@
+<!--  -->
+<template>
+  <div id='Home'>
+      hello world
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home"
+};
+</script>
+<style lang='scss' scoped>
+</style>
