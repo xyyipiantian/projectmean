5905e94549a2da014279e6c2c48221580106e201
xiangtiandi <591102518@qq.com>
2019-09-30 23:17:19 +0800

    2048

---
+++ 2048/main2048.js
@@ -0,0 +1,20 @@
+var board = new Array();
+var score = 0;
+
+$(document).ready(function() {
+    newgame();
+})
+
+fucntion newgame() {
+    //初始化棋盘格
+    init()
+        //再随机生成俩个格子生成数字
+}
+
+function init() {
+    for (var i = 0; i < 4; i++) {
+        for (var j = 0; j < 4; j++) {
+
+        }
+    }
+}
