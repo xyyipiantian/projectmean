431f617abdf6edb2f7ed8d36dc23aeb6ab330dd6
xiangtiandi <591102518@qq.com>
2019-10-11 07:50:58 +0800

    渐隐渐显的导航标题

--- vue去哪网/travel/src/pages/detail/components/Header.vue
+++ vue去哪网/travel/src/pages/detail/components/Header.vue
@@ -1,13 +1,16 @@
 <!--  -->
 <template>
   <div class='header'>
-    <router-link tag="div" to="/" class="header-abs">
+    <router-link tag="div" to="/" class="header-abs" v-show='showAbs'>
       <div class="iconfont header-abs-back">&#xe646;</div>
     </router-link>
-    <div class="header-fixed">
+    <div 
+    class="header-fixed"
+    v-show="!showAbs"
+    :style='opacityStyle'>
       <div>
         景点详情
-        <router-link to="/">
+        <router-link to="/" >
           <div class="iconfont header-fixed-back">&#xe646;</div>
         </router-link>
       </div>
@@ -17,7 +20,35 @@
 
 <script>
 export default {
-  name: "DetailHeader"
+  name: "DetailHeader",
+  data(){
+    return{
+      showAbs:true,
+      opacityStyle:{
+        opacity:0
+      }
+    }
+  },
+  methods:{
+    handleScroll(){
+      const top = document.documentElement.scrollTop;
+      console.log(top)
+      if(top>60){
+        const opacity = top /140;
+        opacity > 1 ? 1 : opacity;
+        this.opacityStyle = {
+          opacity
+        }
+        this.showAbs = false;
+      }else{
+        this.showAbs = true
+        
+      }
+    }
+  },
+  activated(){
+    window.addEventListener('scroll',this.handleScroll)
+  }
 };
 </script>
 <style lang='stylus' scoped>
@@ -30,7 +61,8 @@ export default {
   height: 0.8rem;
   line-height: 0.8rem;
   border-radius: 0.4rem;
-  background: rgba(0, 0, 0, 0.8);
+  background: rgba(0, 0, 0, 0.3);
+  text-align:center;
 
   .header-abs-back {
     color: #fff;
