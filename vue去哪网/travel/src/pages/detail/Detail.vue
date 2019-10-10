40b20b1a562dbdcaf760b6c66e8fe90b19467e4b
xiangtiandi <591102518@qq.com>
2019-10-10 23:56:02 +0800

    Detail的Header

--- vue去哪网/travel/src/pages/detail/Detail.vue
+++ vue去哪网/travel/src/pages/detail/Detail.vue
@@ -2,18 +2,24 @@
 <template>
   <div>
       <Banner></Banner>
+      <DetailHeader></DetailHeader>
+      <div class="content"></div>
   </div>
 </template>
 
 <script>
 import Banner from './components/Banner'
+import DetailHeader from './components/Header'
 export default {
     name:'Detail',
     components:{
         Banner,
+        DetailHeader
     }
 }
 
 </script>
 <style lang='stylus' scoped>
+.content
+    height:50rem
 </style>
---
+++ vue去哪网/travel/src/pages/detail/components/Header.vue
@@ -0,0 +1,56 @@
+<!--  -->
+<template>
+  <div class='header'>
+    <router-link tag="div" to="/" class="header-abs">
+      <div class="iconfont header-abs-back">&#xe646;</div>
+    </router-link>
+    <div class="header-fixed">
+      <div>
+        景点详情
+        <router-link to="/">
+          <div class="iconfont header-fixed-back">&#xe646;</div>
+        </router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DetailHeader"
+};
+</script>
+<style lang='stylus' scoped>
+@import '~styles/varibles.styl'
+.header-abs {
+  position: absolute;
+  left: 0.2rem;
+  top: 0.2rem;
+  width: 0.8rem;
+  height: 0.8rem;
+  line-height: 0.8rem;
+  border-radius: 0.4rem;
+  background: rgba(0, 0, 0, 0.8);
+
+  .header-abs-back {
+    color: #fff;
+    font-size: 0.4rem;
+  }
+}
+.header-fixed 
+    height:$headerHeight
+    line-height:$headerHeight
+    text-align:center
+    color:#fff
+    background:$bgColor
+    position:fixed
+    top:0
+    right:0
+    left:0
+    .header-fixed-back
+        position:absolute;
+        top:0;
+        left:0.2rem;
+        font-size:.4rem;
+        color:#fff
+</style>
